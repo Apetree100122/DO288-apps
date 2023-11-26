48086106557cf8518fccb39aee17c12706fc3efe
Alexander Petree <apetree1001@email.phoenix.edu>
2023-11-25 21:38:07 -0800

    Update and rename mvn-offline-mode.sh to mvn-offline-mode.bash

---
+++ mvn-offline-mode.bash
@@ -0,0 +1,26 @@
+#!/bin
+	/bash
+
+$-rm $-rf_/home/student/DO288
+
+$-mkdir 
+/home/student/DO288
+
+$cd_/home/student/DO288
+
+$git
+
+$-clone https://git.ocp4.example.com/developer/DO288-apps.git
+
+$cd _/home/student/DO288/DO288-apps
+
+$find  
+-name
+
+$pom.xml 
+
+\ 
+$-execdir 
+$ opt-apache-maven/
+$bin
+{{/$mvn_clean_dependency:go-offline $test:Dmaven.test.failure.Falsepackage \;}}
